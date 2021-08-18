9b51b2f4b7aafe38c1f2c91429469b66911c373c
mgt <mgt@oi-wiki.org>
2021-08-18 21:50:34 +0800

    feat: adjust graph style

--- src/dot.rs
+++ src/dot.rs
@@ -4,13 +4,20 @@ use anyhow::Result;
 pub fn from_graph(graph: &Graph, curved: bool) -> Result<String> {
     let mut res = "digraph { \n".to_string();
     if !curved {
-        res.push_str("graph [splines=ortho, nodesep=1];\n");
+        res.push_str("graph [splines=polyline];\n");
     }
     for i in graph {
         match i.node_type {
-            GraphNodeType::Start | GraphNodeType::End => res.push_str(
+            GraphNodeType::Start => res.push_str(
                 format!(
                     "D{} [shape=box, style=rounded, label=\"{}\"];\n",
+                    i.id, i.content
+                )
+                .as_str(),
+            ),
+            GraphNodeType::End => res.push_str(
+                format!(
+                    "{{rank = sink; D{} [shape=box, style=rounded, label=\"{}\"];}}\n",
                     i.id, i.content
                 )
                 .as_str(),
@@ -36,17 +43,17 @@ pub fn from_graph(graph: &Graph, curved: bool) -> Result<String> {
     for i in graph {
         match i.node_type {
             GraphNodeType::Start => {
-                res.push_str(format!("D{} -> D{};\n", i.id, i.id + 1).as_str());
+                res.push_str(format!("D{}:s -> D{}:n;\n", i.id, i.id + 1).as_str());
             }
             GraphNodeType::End => res.push_str("}\n"),
             GraphNodeType::Node => match i.jump {
                 Some(id) => res.push_str(format!("D{} -> D{};\n", i.id, id).as_str()),
-                None => res.push_str(format!("D{} -> D{};\n", i.id, i.id + 1).as_str()),
+                None => res.push_str(format!("D{}:s-> D{}:n;\n", i.id, i.id + 1).as_str()),
             },
             GraphNodeType::Choice => {
-                res.push_str(format!("D{} -> D{} [xlabel=\"Y\"];\n", i.id, i.id + 1).as_str());
+                res.push_str(format!("D{}:s -> D{}:n [xlabel=Y];\n", i.id, i.id + 1).as_str());
                 res.push_str(
-                    format!("D{} -> D{} [xlabel=\"N\"];\n", i.id, i.jump.unwrap()).as_str(),
+                    format!("D{}:e -> D{}:n [xlabel=N];\n", i.id, i.jump.unwrap()).as_str(),
                 );
             }
         }
