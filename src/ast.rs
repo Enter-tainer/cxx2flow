af52cfbaacb611a927c9e4bf5a3b124cac591d48
mgt <mgt@oi-wiki.org>
2022-01-01 09:45:24 +0800

    refactor: remove weaks

--- src/ast.rs
+++ src/ast.rs
@@ -1,9 +1,5 @@
-use std::{
-    cell::RefCell,
-    rc::{Rc, Weak},
-};
+use std::{cell::RefCell, rc::Rc};
 
-// TODO: remove Rc RefCell and Weak
 #[derive(Debug)]
 pub enum AstNode {
     Dummy,
@@ -50,69 +46,12 @@ pub enum AstNode {
 }
 #[derive(Debug)]
 pub struct Ast {
-    pub id: usize, // TODO: can be removed?
     pub node: AstNode,
     pub label: Option<Vec<String>>,
-    pub fa: Option<Weak<RefCell<Ast>>>,
-    pub next: Option<Weak<RefCell<Ast>>>,
-    pub prev: Option<Weak<RefCell<Ast>>>,
 }
 
 impl Ast {
-    pub fn new(id: &mut usize, node: AstNode, label: Option<Vec<String>>) -> Ast {
-        *id += 1;
-        Ast {
-            id: *id - 1,
-            node,
-            fa: None,
-            next: None,
-            prev: None,
-            label,
-        }
-    }
-
-    pub fn is_loop(&self) -> bool {
-        matches!(
-            self.node,
-            AstNode::While { .. } | AstNode::DoWhile { .. } | AstNode::For { .. }
-        )
-    }
-}
-
-pub fn filter_id(node: Rc<RefCell<Ast>>, id: usize) -> Option<Rc<RefCell<Ast>>> {
-    if node.borrow().id == id {
-        return Some(node);
-    }
-    match &node.borrow().node {
-        AstNode::Dummy
-        | AstNode::Stat(_)
-        | AstNode::Continue(_)
-        | AstNode::Break(_)
-        | AstNode::Return(_)
-        | AstNode::Goto(_) => None,
-        AstNode::If {
-            body, otherwise, ..
-        } => {
-            let b1 = filter_id(body.clone(), id);
-            if b1.is_some() {
-                return b1;
-            }
-            if let Some(b2) = otherwise {
-                return filter_id(b2.clone(), id);
-            }
-            None
-        }
-        AstNode::While { body, .. }
-        | AstNode::DoWhile { body, .. }
-        | AstNode::For { body, .. }
-        | AstNode::Switch { body, .. } => filter_id(body.clone(), id),
-        AstNode::Compound(b) => {
-            for i in b {
-                if let Some(v) = filter_id(i.clone(), id) {
-                    return Some(v);
-                }
-            }
-            None
-        }
+    pub fn new(node: AstNode, label: Option<Vec<String>>) -> Ast {
+        Ast { node, label }
     }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,4 @@
-use std::{
-    cell::RefCell,
-    rc::{Rc, Weak},
-};
+use std::{cell::RefCell, rc::Rc};
 
 use crate::ast::{Ast, AstNode};
 use crate::error::{Error, Result};
@@ -60,10 +57,8 @@ pub fn parse(content: &[u8], function_name: Option<String>) -> Result<Rc<RefCell
         if func_name != target_function {
             continue;
         }
-        let mut id: usize = 0;
-        let res = parse_stat(&mut id, stats, content)?;
+        let res = parse_stat(stats, content)?;
         remove_dummy(res.clone());
-        set_links(res.clone(), None);
         return Ok(res);
     }
     Err(Error::NotFound("target function"))
@@ -72,9 +67,7 @@ pub fn parse(content: &[u8], function_name: Option<String>) -> Result<Rc<RefCell
 fn remove_dummy(ast: Rc<RefCell<Ast>>) {
     match &mut ast.borrow_mut().node {
         AstNode::If {
-            cond,
-            body,
-            otherwise,
+            body, otherwise, ..
         } => {
             remove_dummy(body.clone());
             if let Some(otherwise) = otherwise {
@@ -97,64 +90,26 @@ fn remove_dummy(ast: Rc<RefCell<Ast>>) {
     }
 }
 
-fn set_links(ast: Rc<RefCell<Ast>>, fa: Option<Weak<RefCell<Ast>>>) {
-    ast.borrow_mut().fa = fa;
-    match &ast.borrow_mut().node {
-        AstNode::If {
-            cond,
-            body,
-            otherwise,
-        } => {
-            set_links(body.clone(), Some(Rc::downgrade(&ast)));
-            if let Some(b2) = otherwise {
-                set_links(b2.clone(), Some(Rc::downgrade(&ast)));
-            }
-        }
-        AstNode::While { body, .. }
-        | AstNode::DoWhile { body, .. }
-        | AstNode::For { body, .. }
-        | AstNode::Switch { body, .. } => {
-            set_links(body.clone(), Some(Rc::downgrade(&ast)));
-        }
-        AstNode::Compound(v) => {
-            if v.len() >= 2 {
-                for i in 1..v.len() {
-                    let prev = &v[i - 1];
-                    let next = &v[i];
-                    prev.borrow_mut().next = Some(Rc::downgrade(next));
-                    next.borrow_mut().prev = Some(Rc::downgrade(prev));
-                }
-            }
-            for i in v {
-                set_links(i.clone(), Some(Rc::downgrade(&ast)));
-            }
-        }
-        _ => {}
-    }
-}
-
-fn parse_stat(id: &mut usize, stat: Node, content: &[u8]) -> Result<Rc<RefCell<Ast>>> {
+fn parse_stat(stat: Node, content: &[u8]) -> Result<Rc<RefCell<Ast>>> {
     match stat.kind() {
         "compound_statement" => {
             let mut cursor = stat.walk();
             let mut vec = Vec::new();
             if !cursor.goto_first_child() {
                 return Ok(Rc::new(RefCell::new(Ast::new(
-                    id,
                     AstNode::Compound(Vec::new()),
                     None,
                 ))));
             }
             loop {
                 let node = cursor.node();
-                let ast = parse_stat(id, node, content)?;
+                let ast = parse_stat(node, content)?;
                 vec.push(ast);
                 if !cursor.goto_next_sibling() {
                     break;
                 }
             }
             Ok(Rc::new(RefCell::new(Ast::new(
-                id,
                 AstNode::Compound(vec),
                 None,
             ))))
@@ -175,12 +130,12 @@ fn parse_stat(id: &mut usize, stat: Node, content: &[u8]) -> Result<Rc<RefCell<A
                     break;
                 }
             }
-            let ast = parse_stat(id, cursor.node(), content)?;
+            let ast = parse_stat(cursor.node(), content)?;
             ast.borrow_mut().label = Some(label_vec);
             Ok(ast)
         }
         _ => {
-            let res = parse_single_stat(id, stat, content);
+            let res = parse_single_stat(stat, content);
             if let Ok(res) = res {
                 return Ok(res);
             }
@@ -188,7 +143,7 @@ fn parse_stat(id: &mut usize, stat: Node, content: &[u8]) -> Result<Rc<RefCell<A
                 if !matches!(msg, Error::GarbageToken(_)) {
                     return Err(msg);
                 } else {
-                    return Ok(Rc::new(RefCell::new(Ast::new(id, AstNode::Dummy, None))));
+                    return Ok(Rc::new(RefCell::new(Ast::new(AstNode::Dummy, None))));
                 }
             }
             unreachable!();
@@ -196,36 +151,32 @@ fn parse_stat(id: &mut usize, stat: Node, content: &[u8]) -> Result<Rc<RefCell<A
     }
 }
 
-fn parse_single_stat(id: &mut usize, stat: Node, content: &[u8]) -> Result<Rc<RefCell<Ast>>> {
+fn parse_single_stat(stat: Node, content: &[u8]) -> Result<Rc<RefCell<Ast>>> {
     match stat.kind() {
         "continue_statement" => Ok(Rc::new(RefCell::new(Ast::new(
-            id,
             AstNode::Continue("continue".to_string()),
             None,
         )))),
         "break_statement" => Ok(Rc::new(RefCell::new(Ast::new(
-            id,
             AstNode::Break("break".to_string()),
             None,
         )))),
         "return_statement" => {
             let str = stat.utf8_text(content)?;
             Ok(Rc::new(RefCell::new(Ast::new(
-                id,
                 AstNode::Return(String::from(str)),
                 None,
             ))))
         }
-        "if_statement" => parse_if_stat(id, stat, content),
-        "while_statement" => parse_while_stat(id, stat, content),
-        "do_statement" => parse_do_while_stat(id, stat, content),
-        "for_statement" => parse_for_stat(id, stat, content),
-        "switch_statement" => parse_switch_stat(id, stat, content),
-        "goto_statement" => parse_goto_stat(id, stat, content),
+        "if_statement" => parse_if_stat(stat, content),
+        "while_statement" => parse_while_stat(stat, content),
+        "do_statement" => parse_do_while_stat(stat, content),
+        "for_statement" => parse_for_stat(stat, content),
+        "switch_statement" => parse_switch_stat(stat, content),
+        "goto_statement" => parse_goto_stat(stat, content),
         "expression_statement" | "declaration" => {
             let str = stat.utf8_text(content)?;
             Ok(Rc::new(RefCell::new(Ast::new(
-                id,
                 AstNode::Stat(String::from(str)),
                 None,
             ))))
@@ -235,21 +186,20 @@ fn parse_single_stat(id: &mut usize, stat: Node, content: &[u8]) -> Result<Rc<Re
     }
 }
 
-fn parse_if_stat(id: &mut usize, if_stat: Node, content: &[u8]) -> Result<Rc<RefCell<Ast>>> {
+fn parse_if_stat(if_stat: Node, content: &[u8]) -> Result<Rc<RefCell<Ast>>> {
     let condition = if_stat.child_by_field_name("condition").unwrap();
     let blk1 = if_stat.child_by_field_name("consequence");
     let blk2 = if_stat.child_by_field_name("alternative");
     let cond_str = condition.utf8_text(content)?;
-    let body = parse_stat(id, blk1.unwrap(), content)?;
+    let body = parse_stat(blk1.unwrap(), content)?;
 
     let otherwise = if blk2.is_some() {
-        Some(parse_stat(id, blk2.unwrap(), content)?)
+        Some(parse_stat(blk2.unwrap(), content)?)
     } else {
         None
     };
 
     let res = Rc::new(RefCell::new(Ast::new(
-        id,
         AstNode::If {
             cond: String::from(cond_str),
             body,
@@ -260,14 +210,13 @@ fn parse_if_stat(id: &mut usize, if_stat: Node, content: &[u8]) -> Result<Rc<Ref
     Ok(res)
 }
 
-fn parse_while_stat(id: &mut usize, while_stat: Node, content: &[u8]) -> Result<Rc<RefCell<Ast>>> {
+fn parse_while_stat(while_stat: Node, content: &[u8]) -> Result<Rc<RefCell<Ast>>> {
     let condition = while_stat.child_by_field_name("condition").unwrap();
     let body = while_stat.child_by_field_name("body");
     let cond_str = condition.utf8_text(content)?;
-    let body = parse_stat(id, body.unwrap(), content)?;
+    let body = parse_stat(body.unwrap(), content)?;
 
     let res = Rc::new(RefCell::new(Ast::new(
-        id,
         AstNode::While {
             cond: String::from(cond_str),
             body,
@@ -312,11 +261,7 @@ fn get_case_child_and_label<'a>(
     )
 }
 
-fn parse_switch_stat(
-    id: &mut usize,
-    switch_stat: Node,
-    content: &[u8],
-) -> Result<Rc<RefCell<Ast>>> {
+fn parse_switch_stat(switch_stat: Node, content: &[u8]) -> Result<Rc<RefCell<Ast>>> {
     let condition = switch_stat.child_by_field_name("condition").unwrap();
     let body = switch_stat.child_by_field_name("body").unwrap();
     let cond_str = condition.utf8_text(content)?;
@@ -333,7 +278,7 @@ fn parse_switch_stat(
             let mut cursor = child;
             let first_idx = vec_stat.len();
             loop {
-                let stat = parse_stat(id, cursor.node(), content)?;
+                let stat = parse_stat(cursor.node(), content)?;
                 vec_stat.push(stat);
                 if !cursor.goto_next_sibling() {
                     break;
@@ -348,13 +293,8 @@ fn parse_switch_stat(
             break;
         }
     }
-    let inner = Rc::new(RefCell::new(Ast::new(
-        id,
-        AstNode::Compound(vec_stat),
-        None,
-    )));
+    let inner = Rc::new(RefCell::new(Ast::new(AstNode::Compound(vec_stat), None)));
     let res = Rc::new(RefCell::new(Ast::new(
-        id,
         AstNode::Switch {
             cond: String::from(cond_str),
             body: inner,
@@ -364,29 +304,23 @@ fn parse_switch_stat(
     Ok(res)
 }
 
-fn parse_goto_stat(id: &mut usize, goto_stat: Node, content: &[u8]) -> Result<Rc<RefCell<Ast>>> {
+fn parse_goto_stat(goto_stat: Node, content: &[u8]) -> Result<Rc<RefCell<Ast>>> {
     let label_str = goto_stat
         .child_by_field_name("label")
         .unwrap()
         .utf8_text(content)?;
     Ok(Rc::new(RefCell::new(Ast::new(
-        id,
         AstNode::Goto(label_str.to_owned()),
         None,
     ))))
 }
 
-fn parse_do_while_stat(
-    id: &mut usize,
-    do_while_stat: Node,
-    content: &[u8],
-) -> Result<Rc<RefCell<Ast>>> {
+fn parse_do_while_stat(do_while_stat: Node, content: &[u8]) -> Result<Rc<RefCell<Ast>>> {
     let condition = do_while_stat.child_by_field_name("condition").unwrap();
     let body = do_while_stat.child_by_field_name("body");
     let cond_str = condition.utf8_text(content)?;
-    let body = parse_stat(id, body.unwrap(), content)?;
+    let body = parse_stat(body.unwrap(), content)?;
     let res = Rc::new(RefCell::new(Ast::new(
-        id,
         AstNode::DoWhile {
             cond: String::from(cond_str),
             body,
@@ -397,7 +331,7 @@ fn parse_do_while_stat(
     Ok(res)
 }
 
-fn parse_for_stat(id: &mut usize, for_stat: Node, content: &[u8]) -> Result<Rc<RefCell<Ast>>> {
+fn parse_for_stat(for_stat: Node, content: &[u8]) -> Result<Rc<RefCell<Ast>>> {
     let mut cursor = for_stat.walk();
     let init = for_stat.child_by_field_name("initializer");
     let cond = for_stat.child_by_field_name("condition");
@@ -419,9 +353,8 @@ fn parse_for_stat(id: &mut usize, for_stat: Node, content: &[u8]) -> Result<Rc<R
     }
     cursor.goto_first_child();
     while cursor.goto_next_sibling() {}
-    let body = parse_stat(id, cursor.node(), content)?;
+    let body = parse_stat(cursor.node(), content)?;
     let res = Rc::new(RefCell::new(Ast::new(
-        id,
         AstNode::For {
             init: init_str,
             cond: cond_str,
